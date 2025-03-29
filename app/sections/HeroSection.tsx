3de89c0eff67f4c92f9b766e5d67f5be155e420b
İbrahim Eren Coşkun <ibrahimerencskn4@gmail.com>
2025-03-29 04:16:18 +0300

    build safe

--- app/sections/HeroSection.tsx
+++ app/sections/HeroSection.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { heroData } from '../constants';
 import { motion } from 'framer-motion';
--- app/sections/ProjectsSection.tsx
+++ app/sections/ProjectsSection.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import Image from 'next/image';
 import { projects, projectCategories, projectsData } from '../constants';
 import { motion, AnimatePresence } from 'framer-motion';
 
